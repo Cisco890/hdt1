d03821f4254cee39d88bd30e8f8434a1fea6f8ee
Anthony Lou Schwank <anthonylouschwank@gmail.com>
2024-01-15 18:31:59 -0600

    Interfaz

--- IRadio.java
+++ IRadio.java
@@ -1,11 +1,2 @@
 package hdt1;
 
-interface IRadio {
-    void saveStation (int buttonId, double station );
-    boolean isAM();
-    boolean isON();
-    double selectStation(int buttonId);
-    void SwitchOnOff();
-    void SwitchAMFM();
-    double nextStation();
-}
