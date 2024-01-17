9bc641d75d54b3aeb3dd373de8f4175d119a97c6
Anthony Lou Schwank <anthonylouschwank@gmail.com>
2024-01-16 20:15:12 -0600

    Agregadp javadoc a la interfaz

--- IRadio.java
+++ IRadio.java
@@ -1,10 +1,10 @@
 interface IRadio {
-    void saveStation (int buttonId, double station );
-    boolean isAM();
-    boolean isON();
-    double selectStation(int buttonId);
-    void SwitchOnOff();
-    void SwitchAMFM();
-    double nextStation();
+    void saveStation (int buttonId, double station ); //Metodo que servira para guardar una estacion a base del boton a el cual sera guardado y el numero de la estacion
+    boolean isAM(); //Nos dice en que frecuencia esta la radio
+    boolean isON(); //Nos dice si la radio esta encendida o apagada
+    double selectStation(int buttonId); //Metodo que servira para seleccionar el boton en donde se guardo una emisora
+    void SwitchOnOff(); //Enciende o apaga la radio, solo con instanciar el metodo hara el contrario del estado actual
+    void SwitchAMFM(); //Cambia la frecuencia a la que la radio esta funcionando
+    double nextStation(); //Cambia a la siguiente estacion, cabe resaltar que habra una diferenciacion de cuantos numeros avanzara a base de la frecuencia en la que se encuentre la radio
 }
 
